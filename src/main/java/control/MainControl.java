1399f432c562715d5cbcacf6ab99f5a91cb66027
Mathieu Arakelian <mathieuarakeian193@gmail.com>
2026-03-19 11:54:34 +0100

    ajout code maincontrol

--- src/main/java/control/MainControl.java
+++ src/main/java/control/MainControl.java
@@ -4,10 +4,21 @@
  */
 package control;
 
+import view.MainView;
+
 /**
  *
  * @author m.arakelian
  */
 public class MainControl {
+    
+    private MainView mainView;
+    private CommuneControl communeControl;
+    
+    public MainControl(MainView mainView){
+        this.mainView = mainView;
+        CommuneControl communeControl = new CommuneControl(mainView.getCommuneView());
+        UtilisateurControl utilisateurControl = new UtilisateurControl(mainView.getUtilisateurView());
+    }
     
 }
